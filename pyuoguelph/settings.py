1d329afcc39a8006e7c7f04d7072ae68be31efdf
Nick Presta <nick@nickpresta.ca>
2011-03-08 22:27:02 -0600

    Adding required settings file and shell script to set up paths

---
+++ pyuoguelph/settings.py
@@ -0,0 +1,12 @@
+""" This file stores settings for courseparser and other scripts """
+
+# Storage solution for Shove
+# Possible options are:
+#   simple, file, memory, dbm, bsddb, sqlite, mssql, postgres, firebird, mysql,
+#   oracle, svn, s3, zodb, ftp, durus
+store = "memory://"
+# Cache solution for Shove
+# Possible options are:
+#   simple, memory, fileru, file, memcached, bsddb, mssql, postgres, firebird,
+#   mysql, oracle
+cache = "memory://"
---
+++ pyuoguelph/setup_paths.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# This sets up the paths needed for our project
+
+export PYTHONPATH="`pwd`:$PYTHONPATH"
