6361807c1229ef5bce63fd21efe624382254c568
Nick Presta <nick1presta@gmail.com>
2011-09-05 21:39:39 -0400

    Small cleanups, documentation, and tests

--- pyuoguelph/courseparser.py
+++ pyuoguelph/courseparser.py
@@ -13,12 +13,14 @@ except ImportError:
     print("You must have Beautiful Soup installed. Exiting...")
     sys.exit(1)
 
+# If you don't have Shove, we just use a simple dictionary
+# See: __init__ for CourseParser
 try:
     from shove import Shove
+    SHOVE_IMPORTED = True
 except ImportError:
-    import sys
-    print("You must have Shove installed. Exiting...")
-    sys.exit(1)
+    SHOVE_IMPORTED = False
+    print("Shove not installed. No caching available.")
 
 import settings
 
@@ -28,6 +30,12 @@ __license__ = "GPL"
 __version__ = "1.0.0"
 __maintainer__ = "Nicholas Presta"
 __email__ = "nick@nickpresta.ca"
+
+class CourseNotFoundException(Exception):
+    pass
+
+class CourseParserConnectionException(Exception):
+    pass
 
 class CourseParser(object):
     """ This class is responsible for grabbing course data for a specific year
@@ -39,28 +47,56 @@ class CourseParser(object):
         1. Grab all courses available for a specific year
         2. Grab all courses available for a specific program in a specific year """
 
-    CALENDAR_TYPES = ('undergraduate', 'graduate', 'diploma', 'guelphhumber')
     UNDERGRADUATE_CALENDAR = 'undergraduate'
     GRADUATE_CALENDAR = 'graduate'
     DIPLOMA_CALENDAR = 'diploma'
     GUELPHHUMBER_CALENDAR = 'guelphhumber'
+    CALENDAR_TYPES = (UNDERGRADUATE_CALENDAR, GRADUATE_CALENDAR,
+            DIPLOMA_CALENDAR, GUELPHHUMBER_CALENDAR)
 
     def __init__(self, store=settings.store, cache=settings.cache):
-        """ Assigns the default settings to the local settings so they can be
-            easily overridden at runtime """
+        """Assigns the default settings to the local settings so they can be
+            easily overridden at runtime.
+
+            Arguments:
+                store: Your backend store. By default, uses what ever is in
+                       settings.store. If you don't have Shove installed,
+                       will use a plain Python dictionary.
+                cache: Your cache store. By default, uses what ever is in
+                       settings.cache. If you don't have shove installed,
+                       no cache will exist."""
         # This is the URL for a course calendar
         # Use %s as placeholders for dynamic information
         # In this case, the %s are for the calendar type, year and course code
         self.DESC_URL = "http://www.uoguelph.ca/registrar/calendars/%s/%s/courses/%s.shtml"
-        self.store = Shove(store, cache)
+        if SHOVE_IMPORTED:
+            self.store = Shove(store, cache)
+        else:
+            self.store = {}
 
     def get_course(self, year, code, calendar_type=UNDERGRADUATE_CALENDAR,
             fresh=False):
-        """ Returns the course description, the semester it was offered, the
-            credit value, and the restrictions (if any) """
+        """Returns the course description, the semester it was offered, the
+           credit value, and the restrictions (if any).
+
+           Returns:
+               A dictionary containing various pieces of information about a
+               course. Keys are always present, regardless of the data
+               existing or not.
+
+           Arguments:
+               year: Calendar year (i.e. 2011).
+               code: Course code (i.e. cis1910).
+               calendar_type: The calendar type (see
+                              CourseParser.CALENDAR_TYPES for valid options.
+               fresh: Whether or not to fetch the data from your cache,
+                      if possible.
+
+            Exceptions:
+                CourseNotFoundException: When the course is not found for a given
+                                         calendar year or type."""
         key = self._build_key(year, code, calendar_type)
         if key in self.store and fresh:
-            print("returning now")
             return self.store[key]
 
         source = self._fetch_source(self._build_url(year, code, calendar_type))
@@ -103,32 +139,61 @@ class CourseParser(object):
         return info
 
     def _build_key(self, year, code, calendar_type):
-        """ Returns a (hopefully) unique key for our store """
+        """Returns a (hopefully) unique key for our store.
+
+           Returns a unique key for a given calendar year/type and
+           course code.
+
+           Arguments:
+               year: The calendar year (i.e. 2011).
+               code: The course code (i.e. cis1910).
+               calendar_type: The calendar type (see
+                              CourseParser.CALENDAR_TYPES for valid options."""
         return str(year) + str(code) + str(calendar_type)
 
     def _build_year_string(self, year):
-        """ Returns the year in a format that can be used directly in the URL
+        """Returns the year in a format that can be used directly in the URL
             The format is either 'current' for the current year, or
-            the 'year - year+1' """
+            the 'year - year+1'.
+
+           Returns:
+               The year needed for a given calendar (current, if the current year,
+               or a range, like 1999-2000 if in the past)."""
         if int(year) == int(datetime.now().year):
             return 'current'
         else:
             return "%s-%s" % (year, str(int(year) + 1))
 
     def _build_url(self, year, code, calendar_type):
-        """ Returns the full URL based on the uoguelph.ca domain, year, and
-            course code """
+        """Returns the full URL based on the uoguelph.ca domain, year, and
+            course code.
+
+            Arguments:
+                year: The calendar year
+                code: The course code (i.e. cis1910)
+                calendar_type: The calendar type (see
+                               CourseParser.CALENDAR_TYPES for valid options."""
         year = self._build_year_string(year)
         return self.DESC_URL % (calendar_type, year, code.lower())
 
     def _fetch_source(self, url):
-        """ Fetches the source code from the URL given """
+        """Fetches the source code from the given URL.
+
+           Returns: Data as a string from the course page.
+
+           Arguements:
+               url: URL to fetch (generated from other private methods.
+
+           Exceptions:
+               CourseParserConnectionException: Could not connect (urllib2 error).
+               CourseNotFoundException: Could not find course (invalid input)."""
         try:
             site = urllib2.urlopen(url)
-        except urllib2.HTTPError:
-            print("HTTP Error. Try again later")
+        except (urllib2.HTTPError, IOError):
+            raise CourseParserConnectionException("Could not fetch course information"
+                    " at %s" % url)
         if url != site.geturl():
-            raise Exception("Course/year not found at %s" % url)
+            raise CourseNotFoundException("Course/year not found at %s" % url)
         data = site.read()
         site.close()
         return data
--- pyuoguelph/tests/courseparser_test.py
+++ pyuoguelph/tests/courseparser_test.py
@@ -3,32 +3,71 @@ import os
 import unittest
 from datetime import datetime
 
+import courseparser
 from courseparser import CourseParser
+from courseparser import CourseNotFoundException
+from courseparser import CourseParserConnectionException
+
+# save the reference to the real urlopen
+real_urlopen = courseparser.urllib2.urlopen
 
 class KnownUndergraduateCourseNumbers(unittest.TestCase):
     known_course_numbers = (
             ('cis1910', '1910'),
             ('cis2750', '2750'),
             ('cis3000', '3000'))
-    course_parser = CourseParser()
+
+    def setUp(self):
+        self.course_parser = CourseParser()
+        courseparser.urllib2.urlopen = real_urlopen
 
     def testCourseNumberKnownValues(self):
         for course, code in self.known_course_numbers:
             result = self.course_parser.get_course(datetime.now().year, course)
             self.assertEqual(result['course_number'], code)
 
+    def testCourseNotFoundException(self):
+        self.assertRaises(CourseNotFoundException,
+                self.course_parser.get_course, '2011', 'cis1234')
+
+    def testCourseParserConnectionException(self):
+        # Mock urllib2's urlopen to throw an exception
+        def dummy_urlopen(url):
+            raise IOError
+        courseparser.urllib2.urlopen = dummy_urlopen
+
+        self.assertRaises(CourseParserConnectionException,
+                self.course_parser.get_course, '2099', 'cis1910')
+
 class KnownGuelphHumberCourseNumbers(unittest.TestCase):
     known_course_numbers = (
             ('psyc1130', '1130'),
             ('kin2060', '2060'),
             ('ecs4900', '4900'))
-    course_parser = CourseParser()
+
+    def setUp(self):
+        self.course_parser = CourseParser()
+        courseparser.urllib2.urlopen = real_urlopen
 
     def testCourseNumberKnownValues(self):
         for course, code in self.known_course_numbers:
             result = self.course_parser.get_course(datetime.now().year, course,
                     CourseParser.GUELPHHUMBER_CALENDAR)
             self.assertEqual(result['course_number'], code)
+
+    def testCourseNotFoundException(self):
+        self.assertRaises(CourseNotFoundException,
+                self.course_parser.get_course, '2011', 'psyc9999')
+
+    def testCourseParserConnectionException(self):
+        # Mock urllib2's urlopen to throw an exception
+        def dummy_urlopen(url):
+            raise IOError
+        courseparser.urllib2.urlopen = dummy_urlopen
+
+        self.assertRaises(CourseParserConnectionException,
+                self.course_parser.get_course, '2099', 'psyc1130')
+
 
 if __name__ == '__main__':
     unittest.main()
