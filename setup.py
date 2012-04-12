d853c1ad0923342376d554b1c9f63f7fd4bccca9
Nick Presta <nick@nickpresta.ca>
2012-04-11 21:34:42 -0400

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name="pyuoguelph",
+    license="BSD",
+    version="0.1",
+    description="Python bindings to University of Guelph services",
+    author="Nick Presta",
+    maintainer="Nick Presta",
+    maintainer_email="nick@nickpresta.ca",
+    url="https://github.com/NickPresta/pyuoguelph",
+    py_modules=['courseparser']
+    )
--- setup_paths.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# This sets up the paths needed for our project
-
-export PYTHONPATH="`pwd`:$PYTHONPATH"
